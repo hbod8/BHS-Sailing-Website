c1fae66c40edb527ceaafd922958f589e4506596
Harrison Saliba <harrysaliba4139@gmail.com>
2016-05-18 21:03:55 -0700

    Quick fix

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
             $pass = $_POST["pass"];
             if (sha1($pass) == '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8')
             {
+                echo '<p>logging in</p>';
                 include form.php;
             }
         }
