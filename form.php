58c3adb31be4840fa35338a54b86fd42dcd610bd
Harrison Saliba <harrysaliba4139@gmail.com>
2016-05-18 21:26:57 -0700

    forgot to start session

--- form.php
+++ form.php
@@ -2,12 +2,16 @@
     <head></head>
     <body>
         <?php
+        
+        session_start();
+        
         if (sha1($_SESSION['pass']) != '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8')
         {
             exit('bad passkey');
             session_unset();
             session_destroy();
         }
+        echo '<p>logged in</p>'
         ?>
     </body>
 </html>
