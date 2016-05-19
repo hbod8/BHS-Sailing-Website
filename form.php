4b86149deade42ed8c7ccb0683028bcede8725ae
Harrison Saliba <harrysaliba4139@gmail.com>
2016-05-18 21:24:58 -0700

    quick fix

--- form.php
+++ form.php
@@ -2,7 +2,7 @@
     <head></head>
     <body>
         <?php
-        if ($_SESSION['pass'] != '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8')
+        if (sha1($_SESSION['pass']) != '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8')
         {
             exit('bad passkey');
             session_unset();
