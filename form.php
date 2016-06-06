5b9348311e706d80d3149c0fa542c13f15ea0684
Harrison Saliba <harrysaliba4139@gmail.com>
2016-06-06 11:23:25 -0700

    Testing php XML editing

--- form.php
+++ form.php
@@ -2,31 +2,40 @@
     <head></head>
     <body>
         <?php
-            //read stored key
-            $keyFile = fopen("secure/key.txt", "r") or die("Unable to get hash.");
-            $key = fread($keyFile, filesize("secure/key.txt"));
-            fclose($keyfile);
-            //Init session to keep varibles between pages
-            session_start();
-            //process submitted keys and compare
-            if (sha1($_SESSION['pass']) != $key)
-            {
-                exit('bad passkey');
-                session_unset();
-                session_destroy();
-            }
-            echo '<p>logged in</p>';
-            // checks if user presses logout button, if true, then clear session varibles
-            if ($_POST['logout'])
-            {
-                session_unset();
-            }
-
-            /* form */
-
-            $newsFile = fopen("news.xml", "r");
+        //read stored key
+        $keyFile = fopen("secure/key.txt", "r") or die("Unable to get hash.");
+        $key = fread($keyFile, filesize("secure/key.txt"));
+        fclose($keyfile);
+        //Init session to keep varibles between pages
+        session_start();
+        //process submitted keys and compare
+        if (sha1($_SESSION['pass']) != $key)
+        {
+            exit('bad passkey');
+            session_unset();
+            session_destroy();
+        }
+        echo '<p>logged in</p>';
+        // checks if user presses logout button, if true, then clear session varibles
+        if ($_POST['logout'])
+        {
+            session_unset();
+        }
+        /* Form submission */
+        $XMLFileRead = fopen("newsdata.xml", "r") or die ("Cannot open newsdata.xml for reading.");
+        $XMLWriteFile = fopen("newsdata.xml", "a") or die ("Cannot open newsdata.xml for writing.");
+        $FileContents = file($XMLFileRead);
+        //testing
+        echo $FileContents[0] . "<br>" . $FileContents[1] . "<br>" . $FileContents[2] . "<br>";
+        //testing end
+        $NewFile = "";
+        for ($i = 0; $i < count($FileContents) - 1; $i++)
+        {
+            $NewFile += $FileContents[$i];
+        }
+        echo "finalfile:" . $NewFile;
         ?>
-        <form>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="get">
             Title:<br>
             <input type="text" name="title">
             Post:<br>
