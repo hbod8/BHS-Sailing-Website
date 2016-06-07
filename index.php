94e04b5dbad59ab508d3fe64b3437f8a76756b45
Harrison Saliba <harrysaliba4139@gmail.com>
2016-06-07 00:00:03 -0700

    seems like i forgot how to html also, more coffee!

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                 echo "<h2>" . $announcement->title . "</h2>";
                 echo "<p id=\"author\">" . $announcement->author . "</p>";
                 echo "<p id=\"date\">" . $announcement->date->event . "</p>";
-                echo "<p id=\"nc\">" . $announcement->content . "<p>";
+                echo "<p id=\"nc\">" . $announcement->content . "</p>";
                 foreach ($announcement->media->image as $image)
                 {
                     echo "<image id=\"newsimage\" src=\"" . $image->url . "\">";
