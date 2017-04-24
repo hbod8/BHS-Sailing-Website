bb6bb27168c5a46c9beb6ca5f613656b76fd9837
Harrison Saliba <harrysaliba4139@gmail.com>
2017-04-24 12:20:58 -0700

    Added dynamic elements.
    navbar/footer now universal.

---
+++ include/navbar.php
@@ -0,0 +1,11 @@
+echo '
+<div id="navitem"><a id="navlink" href="news.php"><p id="navtext">News</p></a></div>
+<div id="navitem"><a id="navlink" href="team.html"><p id="navtext">Team</p></a></div>
+<div id="navitem"><a id="navlink" href="coaches.html"><p id="navtext">Coaches</p></a></div>
+<div id="navitem"><a id="navlink" href="equipment.html"><p id="navtext">Equipment</p></a></div>
+<div id="navitem"><a id="navlink" href="calender.html"><p id="navtext">Calendar</p></a></div>
+<div id="navitem"><a id="navlink" href="practice.html"><p id="navtext">Practice</p></a></div>
+<div id="navitem"><a id="navlink" href="friends.html"><p id="navtext">Friends</p></a></div>
+<div id="navitem"><a id="navlink" href="help.html"><p id="navtext">How to help</p></a></div>
+<div id="navitem"><a id="navlink" href="signup.html"><p id="navtext">Sign Up</p></a></div>
+'
