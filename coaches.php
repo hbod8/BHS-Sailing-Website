033c115bd5c553b411fc9d98cd9366cbab54feb3
Harrison Saliba <harrysaliba4139@gmail.com>
2017-04-25 13:59:13 -0700

    Page updates.
    Had to remove broken links.

--- coaches.php
+++ coaches.php
@@ -23,7 +23,7 @@
             <p>The Coaches:</p>
             <p>Susan Kaseler - Susie doesn't like to talk about her accomplishments but wrote the following:</p>
             <p>"I was 17 when I began coaching, 10 when I started racing my own boat. As kids we raced 7 days a week in junior and adult regattas all summer, and in the winter crewed for the best sailors in the country (we broke up the ice in the harbor with a launch that had a door strapped on the bow)."</p>
-            <p><a href="http://bhssailing.net/wp-content/uploads/2013/04/SusieKaselerBio.pdf">Read the bio that the US Olympic Committee wrote</a> when they awarded Susie the Coach-of-the-year.</p>
+<!--            <p><a href="http://bhssailing.net/wp-content/uploads/2013/04/SusieKaselerBio.pdf">Read the bio that the US Olympic Committee wrote</a> when they awarded Susie the Coach-of-the-year.</p>-->
         </div>
         <footer>
             <?php
